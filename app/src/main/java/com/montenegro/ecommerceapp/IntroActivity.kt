7e40c18cc4d42838f95db68d2cf3f2d62411c1e3
Montenegro Cruz <montenegrocruz.uk@icloud.com>
2024-12-20 23:14:52 +0000

    Splash Activity Ready

---
+++ app/src/main/java/com/montenegro/ecommerceapp/IntroActivity.kt
@@ -0,0 +1,100 @@
+package com.montenegro.ecommerceapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+/**
+ * IntroActivity es una actividad que muestra una pantalla de introducción
+ * utilizando Jetpack Compose. Al hacer clic en la imagen final, se inicia
+ * MainActivity.
+ */
+class IntroActivity : BaseActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      IntroScreen(
+        onClick = {
+          startActivity(Intent(this, MainActivity::class.java))
+        }
+      )
+    }
+  }
+}
+
+/**
+ * IntroScreen es una función composable que muestra una pantalla de introducción
+ * con varias imágenes. La última imagen es clicable y ejecuta la función onClick
+ * cuando se hace clic en ella.
+ *
+ * @param onClick Función lambda que se ejecuta cuando se hace clic en la última imagen.
+ */
+@Preview(
+  showBackground = true,
+  showSystemUi = true,
+  device = "spec:width=411dp,height=891dp,dpi=420"
+)
+@Composable
+fun IntroScreen(onClick: () -> Unit = {}) {
+  Image(
+    painter = painterResource(id = R.drawable.background),
+    contentDescription = null,
+    modifier = Modifier
+      .background(Color.White)
+      .fillMaxSize(),
+    contentScale = ContentScale.Crop
+  )
+  
+  Column(
+    modifier = Modifier
+      .fillMaxSize()
+      .verticalScroll(rememberScrollState())
+      .padding(16.dp),
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Image(
+      painter = painterResource(id = R.drawable.fashion),
+      contentDescription = null,
+      modifier = Modifier
+        .padding(top = 8.dp)
+        .fillMaxSize(),
+      contentScale = ContentScale.Fit
+    )
+    Image(
+      painter = painterResource(id = R.drawable.title),
+      contentDescription = null,
+      modifier = Modifier
+        .padding(top = 16.dp)
+        .fillMaxWidth(),
+      contentScale = ContentScale.Fit
+    )
+    Image(
+      painter = painterResource(id = R.drawable.go),
+      contentDescription = null,
+      modifier = Modifier
+        .fillMaxWidth()
+        .clickable { onClick() },
+    )
+  }
+}
